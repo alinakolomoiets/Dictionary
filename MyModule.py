84de180471a802926e899f6ec4026326e1856c22
Alina <93324365+alinakolomoiets@users.noreply.github.com>
2022-01-20 14:13:45 +0200

    Update MyModule.py

--- MyModule.py
+++ MyModule.py
@@ -7,31 +7,49 @@ def failist_lugemine(f:str,l:list):
 	fail.close()
 	return l
 
-def failist_lugemine(f:str,l:list):
-	"""Info failist f listisse l
-	"""	
-	fail=open(f,'r',encoding="utf-8-sig")
-	for rida in fail:
-		l.append(rida.strip())
-	fail.close()
-	return l
-
-def failisse_salvestamine(f:str,l:list):
+def rida_salvestamine(f:str,rida:str):
 	"""Loetelu salvaestame failisse
 	"""	
-	fail=open(f,'w')#перезапись
-	for el in l:
-		fail.write(el+'\n')
+	fail=open(f,'a')#перезапись
+	fail.write(rida+'\n')
 	fail.close()
 
-def uus_sõna(f:str,rida:str)->list:
+def uus_sõna(f:str,rida:str,l:list)->list:
 	"""Üks sõna või lause (rida) salvestame failisse
     :param str f:fail nimetus
     :param str rida : lisatav sõna
     """
-	with open(f,'a',encoding="utf-8-sig") as fail:
-		fail.write(rida+'\n')
+	l=[]
+	with open(f,"a",encoding="utf-8-sig") as fail:
+		fail.write(rida+"\n")
 	l=failist_lugemine(f)
 	return l
+
+def tõlkimine(l1:list,l2:list):
+	sõna=input("Что переводить?")
+	if sõna in l1:
+		tõlk=l2[l1.index(sõna)]
+		print(sõna+"-"+tõlk)
+	elif sõna in l2:
+		tõlk =l1[l2.index(sõna)]
+		print(sõna+"-"+tõlk)
+	else:
+		print("Слова нет")
+
+def correction(sõna:str,l:list):
+	for i in range(len(l)):
+		if l[i]==sõna:
+			uus_sõna=sõna.replace(sõna,input("Новое слово"))
+			l.insert(i,uus_sõna)
+			l.remove(sõna)
+	return l
+
+
+import os
+from gtts import gTTS
+
+def heli(text:str,keel:str):
+	obj=gTTs(text=text,lang=keel,slow=False).save("heli.mp3") 
+	os.system("heli.mp3")
 
 #def kõik_sõna(f:str

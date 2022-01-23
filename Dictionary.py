5e9c94945da0cab824340c14fea31e3a9eaa5734
Alina <93324365+alinakolomoiets@users.noreply.github.com>
2022-01-23 22:53:45 +0300

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -6,7 +6,7 @@ ang=failist_lugemine('ang.txt',ang_list)
 
 print("***Словарь-переводчик с английского на русский и с русского на английский***")
 while True :
-	menu=input("Проговаривать-R,\nНовое слово-H,\nПосмотреть все слова-S\nОшибки-J\n")
+	menu=input("Проговаривать-R,\nНовое слово-H,\nПосмотреть все слова-S\nДобавить слово-J\nПеревод-A\n")
 	
 	if menu.upper()=="H":
 		rus_list=uus_sõna("rus.txt",input("Новое слово:"),rus_list)
@@ -14,12 +14,16 @@ while True :
 	elif menu.upper()=="S":
 		for i in range (len(ang_list)):
 			print(rus_list[i]+"-"+ang_list[i])
-		#print(rus_list)
-		#print(ang_list)
+	elif menu.upper()=="J":
+		correction()
+	elif menu.upper()=="A":
+		tõlkimine()
 	elif menu.upper()=="R":
 		keel=input("Что проговорить?")#ru et en
 		sõna=input("Sõna:")
 		heli(sõna,keel)
 	else:
 		break
+		
+break
 		

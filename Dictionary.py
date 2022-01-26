0b5d1e67456ad8255e0b6c7c59bfba35f5781302
Alina <93324365+alinakolomoiets@users.noreply.github.com>
2022-01-27 01:28:06 +0300

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -6,24 +6,28 @@ ang=failist_lugemine('ang.txt',ang_list)
 
 print("***Словарь-переводчик с английского на русский и с русского на английский***")
 while True :
-	menu=input("Проговаривать-R,\nНовое слово-H,\nПосмотреть все слова-S\nДобавить слово-J\nПеревод-A\n")
+	menu=input("Проговаривать-R,\nНовое слово-H,\nПосмотреть все слова-S\nДобавить слово-W\nПеревод-A\nКонтроль-K\n")
 	
 	if menu.upper()=="H":
 		rus_list=uus_sõna("rus.txt",input("Новое слово:"),rus_list)
 		ang_list=uus_sõna("ang.txt",input("New:"),ang_list)
+
 	elif menu.upper()=="S":
+		#print(rus_list)
+		#print(ang_list)
 		for i in range (len(ang_list)):
 			print(rus_list[i]+"-"+ang_list[i])
-	elif menu.upper()=="J":
+	elif menu.upper()=="W":
 		correction()
 	elif menu.upper()=="A":
-		tõlkimine()
-	elif menu.upper()=="R":
-		keel=input("Что проговорить?")#ru et en
-		sõna=input("Sõna:")
-		heli(sõna,keel)
-	else:
-		break
-		
-break
-		
+		tõlkimine(rus_list,ang_list)
+	elif menu.upper()=="K":
+		kontrol(rus_list,ang_list)
+
+
+	#elif menu.upper()=="R":
+	#	keel=input("Что проговорить?")#ru et en
+	#	sõna=input("Sõna:")
+	#	heli(sõna,keel)
+	#else:
+	#	break	

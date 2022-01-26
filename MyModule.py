be60925927be55f25dc9eb1b1ca95b35b6d59460
Alina <93324365+alinakolomoiets@users.noreply.github.com>
2022-01-27 01:30:31 +0300

    Update MyModule.py

--- MyModule.py
+++ MyModule.py
@@ -1,5 +1,7 @@
+from random import shuffle
+import random
 def failist_lugemine(f:str,l:list):
-	"""Info failist f listisse l
+	"""Информация из файла l
 	"""
 	fail=open(f,'r',encoding="utf-8-sig")#,encoding="utf-8-sig"
 	for rida in fail:
@@ -7,30 +9,23 @@ def failist_lugemine(f:str,l:list):
 	fail.close()
 	return l
 
-def rida_salvestamine(f:str,rida:str):
-	"""Loetelu salvaestame failisse
-	"""	
-	fail=open(f,'a')#перезапись
-	fail.write(rida+'\n')
-	fail.close()
-
 def uus_sõna(f:str,rida:str,l:list)->list:
-	"""Üks sõna või lause (rida) salvestame failisse
-    :param str f:fail nimetus
-    :param str rida : lisatav sõna
+	"""Одно слово или предложение (rida) мы сохраняем в файл
+    :param str f : имя файла 
+    :param str rida : добавить слово
     """
 	l=[]
 	with open(f,"a",encoding="utf-8-sig") as fail:
 		fail.write(rida+"\n")
-	l=failist_lugemine(f)
-	return l
+	#l=failist_lugemine(f)
+	#return l
 
 def tõlkimine(l1:list,l2:list):
-	"""Tõlkimine sõna
-	:param list l1:
-	:param list l2:
+	"""Переводим слова 
+	:param list l1:Список первого языка
+	:param list l2:Список второго языка
 	"""
-	sõna=input("Что переводить?")
+	sõna=input("Введите слово:")
 	if sõna in l1:
 		tõlk=l2[l1.index(sõna)]
 		print(sõna+"-"+tõlk)
@@ -43,15 +38,45 @@ def tõlkimine(l1:list,l2:list):
 def correction(sõna:str,l:list):
 	for i in range(len(l)):
 		if l[i]==sõna:
-			uus_sõna=sõna.replace(sõna,input("Новое слово->"))
+			uus_sona=sõna.replace(sõna,input("Новое слово: "))
 			l.insert(i,uus_sõna)
 			l.remove(sõna)
 	return l
 
+def kontrol(l1:list,l2:list):
+	"""
+	"""
+	punktisumma=0
+	print(1)
+	res=0
+	spisok=[]
+	spisok.extend(l1)
+	spisok.extend(l2)
+	random.shuffle(spisok)
+	print("Random list ",spisok)
+	for i in range(len(l1)):
+		print("l1 ",l1)
+		print("l2 ",l2)
+		h=input(f"Переведи данное слово - '{spisok[i]}': ")
+		if h in l1 or h in l2:
+			if spisok[i] in l1:
+				if l1.index(spisok[i])==l2.index(h):
+					res+=1
+					print('правильно!')
+					print()
+				elif spisok[i] in l2:
+					if l2.index(spisok[i])==l1.index(h):
+						res+=1
+						print('правильно!')
+						print()
+		else:
+			print("Неправильно!")
+			print()
+	p=(punktisumma/len(l1))*100
+	print("Твой ответ"+p)
+#import os
+#from gtts import gTTS
 
-import os
-from gtts import gTTS
-
-def heli(text:str,keel:str):
-	obj=gTTS(text=text,lang=keel,slow=False).save("heli.mp3") 
-	os.system("heli.mp3")
+#def heli(text:str,keel:str):
+#	obj=gTTS(text=text,lang=keel,slow=False).save("heli.mp3") 
+#	os.system("heli.mp3")
